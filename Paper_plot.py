d98c9e0fd1668205e41472e82f1ad33c4607683e
Zhang Jingchuan <57850924+Amgroot-w@users.noreply.github.com>
2021-03-10 16:26:16 +0800

    2021.3.10小论文作图更新

--- Paper_plot.py
+++ Paper_plot.py
@@ -13,12 +13,6 @@ from TE_plot import plot8
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
 
-def savefig(path):
-    plt.savefig(path, bbox_inches='tight')  # 保存图片
-    fig = Image.open(path)
-    fig = fig.convert('L')  # 将RGB图片转为灰度图
-    fig.save(path)  # 保存
-
 def plot1():
     train_ac = 100 * pd.read_csv('train_ac.csv', header=None).values
     test_ac = 100 * pd.read_csv('test_ac.csv', header=None).values
@@ -29,8 +23,8 @@ def plot1():
     ax1.set_xticklabels([i for i in range(0, 22, 2)], fontsize='15')
     ax1.set_xticks([i for i in range(0, 22, 2)])
     ax1.tick_params(labelsize=15)
-    plt.plot([2, 4, 5, 10, 20], train_ac[:, 1], 'o-r', markersize=9, alpha=1, label="训练集正确率")
-    plt.plot([2, 4, 5, 10, 20], test_ac[:, 1], 's-', c='orange', markersize=9, alpha=1, label="测试集正确率")
+    plt.plot([2, 4, 5, 10, 20], train_ac[:, 1], 'o-', c='black', markersize=9, alpha=0.5, label="训练集正确率")
+    plt.plot([2, 4, 5, 10, 20], test_ac[:, 1], 's-', c='black', markersize=9, alpha=0.5, label="测试集正确率")
 
     plt.xlabel('隐含层数', fontsize=15)
     plt.ylabel('正确率（%）', fontsize=15)
@@ -39,11 +33,11 @@ def plot1():
 
     ax2 = ax1.twinx()
     ax2.tick_params(labelsize=15)
-    plt.plot([2, 4, 5, 10, 20], runtime[0, :], '^-k', markersize=12, alpha=0.7, label='运行时间')
+    plt.plot([2, 4, 5, 10, 20], runtime[0, :], '^-', c='black', markersize=9, alpha=0.5, label='运行时间')
     ax2.set_ylabel('运行时间(s)', fontsize=15)
     plt.ylim(45, 84)
     plt.legend(loc='upper right', fontsize=14).get_frame().set_linewidth(0.0)
-    savefig(r'PaperFigs\fig1.png')
+    plt.savefig(r'PaperFigs\fig1.svg', bbox_inches='tight')
     plt.show()
 
 def plot2():
@@ -56,8 +50,8 @@ def plot2():
     ax1.set_xticklabels([16, 18, 20, 22, 24], fontsize='15')
     ax1.set_xticks([16, 18, 20, 22, 24])
     ax1.tick_params(labelsize=15)
-    plt.plot([16, 18, 20, 22, 24], train_ac[3, :], 'o-r', markersize=9, alpha=1, label="训练集正确率")
-    plt.plot([16, 18, 20, 22, 24], test_ac[3, :], 's-', c='orange', markersize=9, alpha=1, label="测试集正确率")
+    plt.plot([16, 18, 20, 22, 24], train_ac[3, :], 'o-', c='black', markersize=9, alpha=0.5, label="训练集正确率")
+    plt.plot([16, 18, 20, 22, 24], test_ac[3, :], 's-', c='black', markersize=9, alpha=0.5, label="测试集正确率")
 
     plt.xlabel('隐层节点数', fontsize=15)
     plt.ylabel('正确率（%）', fontsize=15)
@@ -66,11 +60,11 @@ def plot2():
 
     ax2 = ax1.twinx()
     ax2.tick_params(labelsize=15)
-    plt.plot([16, 18, 20, 22, 24], runtime[0, :], '^-k', markersize=12, alpha=0.7, label='运行时间')
+    plt.plot([16, 18, 20, 22, 24], runtime[0, :], '^-', c='black', markersize=9, alpha=0.5, label='运行时间')
     ax2.set_ylabel('运行时间(s)', fontsize=15)
     plt.ylim(46, 53)
     plt.legend(loc='upper right', fontsize=14).get_frame().set_linewidth(0.0)
-    savefig(r'PaperFigs\fig2.png')
+    plt.savefig(r'PaperFigs\fig2.svg', bbox_inches='tight')
     plt.show()
 
 def plot3():
@@ -78,13 +72,13 @@ def plot3():
     plt.rcParams['xtick.direction'] = 'in'
     plt.rcParams['ytick.direction'] = 'in'
     plt.bar(range(6), runtime, width=0.6, fc='gray')
-    plt.plot(range(6), runtime, 'o--k')
+    plt.plot(range(6), runtime, 'o--', c='black')
     plt.xticks(range(6), ['BP', 'SVM', 'PCA', 'KPCA', 'DAE', 'LSTM-DAE'], fontsize=15)
     plt.yticks(fontsize=15)
     plt.xlabel('模型', fontsize=15)
     plt.ylabel('运行时间 (s)', fontsize=15)
     plt.title('六类模型的运行时间', fontsize=15)
-    savefig(r'PaperFigs\fig3.png')
+    plt.savefig(r'PaperFigs\fig3.svg', bbox_inches='tight')
     plt.show()
 
 def plot4():
@@ -96,12 +90,12 @@ def plot4():
     lstm = [97.24, 97.14, 97.05, 97.66, 97.48]
     plt.rcParams['xtick.direction'] = 'in'
     plt.rcParams['ytick.direction'] = 'in'
-    plt.plot(range(5), bp, '>--', label='BP', markersize=12)
-    plt.plot(range(5), svm, '^--', label='SVM', markersize=12)
-    plt.plot(range(5), pca, 's--c', label='PCA', markersize=12)
-    plt.plot(range(5), kpca, 'D--y', label='KPCA', markersize=11)
-    plt.plot(range(5), dae, 'o--g', label='DAE', markersize=12)
-    plt.plot(range(5), lstm, '*--r', label='LSTM-DAE', markersize=15)
+    plt.plot(range(5), bp, '>--', c='black', label='BP', alpha=0.5, markersize=9)
+    plt.plot(range(5), svm, '^--', c='black', label='SVM', alpha=0.5, markersize=9)
+    plt.plot(range(5), pca, 's--', c='black', label='PCA', alpha=0.5, markersize=9)
+    plt.plot(range(5), kpca, 'D--', c='black', label='KPCA', alpha=0.5, markersize=9)
+    plt.plot(range(5), dae, 'o--', c='black', label='DAE', alpha=0.5, markersize=9)
+    plt.plot(range(5), lstm, '*--', c='black', label='LSTM-DAE', alpha=0.5, markersize=9)
 
     plt.title('六类模型的正确率', fontsize=15)
     plt.xticks(range(5), [1, 2, 3, 4, 5], fontsize=15)
@@ -111,9 +105,10 @@ def plot4():
     plt.xlim(-0.5, 4.5)
     plt.ylim(15, 105)
     plt.legend(fontsize=13).get_frame().set_linewidth(0.0)
-    savefig(r'PaperFigs\fig4.png')
+    plt.savefig(r'PaperFigs\fig4.svg', bbox_inches='tight')
     plt.show()
 
+# 将RGB图1~图4转换为灰度图
 def plot5():
     for i in np.arange(1, 5):
         path = r'PaperFigs\图片%d.png' % i
